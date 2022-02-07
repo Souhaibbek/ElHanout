72d851c17a6432072b63d0a5bd4066dd34287b52
souhaibbek <bekkaysouhaib1996@gmail.com>
2022-02-07 22:25:52 +0100

    add changethememode

--- lib/logic/controller/layoutcontroller.dart
+++ lib/logic/controller/layoutcontroller.dart
@@ -1,12 +1,9 @@
-import 'package:elhanout/utils/theme.dart';
 import 'package:elhanout/view/screens/category.dart';
 import 'package:elhanout/view/screens/favorite.dart';
 import 'package:elhanout/view/screens/home.dart';
 import 'package:elhanout/view/screens/setting.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:get_storage/get_storage.dart';
